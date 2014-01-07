43cab413c898653ae21a85fd6a263735558a9007
Andreas Willinger <awi95@gmx.at>
2014-01-07 20:04:05 +0100

    Cleanup - Nicht benötigte Dateien entfernt - Makefile an obige Änderung angepasst - eg3_impl in corba1_server umbenannt - Windows Makefile entfernt

--- c++/dir.mk
+++ c++/dir.mk
@@ -1,55 +1,16 @@
 
-CXXSRCS = eg1.cc eg2_impl.cc eg2_clt.cc eg3_impl.cc eg3_clt.cc
+CXXSRCS = corba1_server.cc
 
 DIR_CPPFLAGS = $(CORBA_CPPFLAGS)
 
 CORBA_INTERFACES = echo
 
-ifdef OSF1
-ifeq ($(notdir $(CXX)),cxx)
-NoTieExample = 1
-endif
-endif
+corba1_server        = $(patsubst %,$(BinPattern),corba1_server)
 
-ifndef NoTieExample
-# -Wbtp tells the compiler to generate tie implementation template
-OMNIORB_IDL += -Wbtp
-eg3_tieimpl = $(patsubst %,$(BinPattern),eg3_tieimpl)
-endif
-
-
-eg1        = $(patsubst %,$(BinPattern),eg1)
-eg2_impl   = $(patsubst %,$(BinPattern),eg2_impl)
-eg2_clt    = $(patsubst %,$(BinPattern),eg2_clt)
-eg3_impl   = $(patsubst %,$(BinPattern),eg3_impl)
-eg3_clt    = $(patsubst %,$(BinPattern),eg3_clt)
-
-
-all:: $(eg1) $(eg2_impl) $(eg2_clt)  $(eg3_impl) $(eg3_clt) $(eg3_tieimpl)
+all:: $(corba1_server)
 
 clean::
-	$(RM) $(eg1) $(eg2_impl) $(eg2_clt) $(eg3_impl) $(eg3_clt) \
-		$(eg3_tieimpl)
+	$(RM) $(corba1_server)
 
-export:: $(eg1) $(eg2_impl) $(eg2_clt) $(eg3_impl) $(eg3_clt) $(eg3_tieimpl)
-	@(module="echoexamples"; $(ExportExecutable))
-
-$(eg1): eg1.o $(CORBA_STATIC_STUB_OBJS) $(CORBA_LIB_DEPEND)
+$(corba1_server): corba1_server.o $(CORBA_STATIC_STUB_OBJS) $(CORBA_LIB_DEPEND)
 	@(libs="$(CORBA_LIB_NODYN)"; $(CXXExecutable))
-
-$(eg2_impl): eg2_impl.o $(CORBA_STATIC_STUB_OBJS) $(CORBA_LIB_DEPEND)
-	@(libs="$(CORBA_LIB_NODYN)"; $(CXXExecutable))
-
-$(eg2_clt): eg2_clt.o $(CORBA_STATIC_STUB_OBJS) $(CORBA_LIB_DEPEND)
-	@(libs="$(CORBA_LIB_NODYN)"; $(CXXExecutable))
-
-$(eg3_impl): eg3_impl.o $(CORBA_STATIC_STUB_OBJS) $(CORBA_LIB_DEPEND)
-	@(libs="$(CORBA_LIB_NODYN)"; $(CXXExecutable))
-
-$(eg3_clt): eg3_clt.o $(CORBA_STATIC_STUB_OBJS) $(CORBA_LIB_DEPEND)
-	@(libs="$(CORBA_LIB_NODYN)"; $(CXXExecutable))
-
-ifndef NoTieExample
-$(eg3_tieimpl): eg3_tieimpl.o $(CORBA_STATIC_STUB_OBJS) $(CORBA_LIB_DEPEND)
-	@(libs="$(CORBA_LIB_NODYN)"; $(CXXExecutable))
-endif
